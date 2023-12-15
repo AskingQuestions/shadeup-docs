695400faf76dcafe14233d727ace1aaaff64739a
Jeremy Frank <18402381+AskingQuestions@users.noreply.github.com>
2023-12-15 09:38:14 -0500

    UPDATE Discord link

--- svdoc.config.cjs
+++ svdoc.config.cjs
@@ -1,52 +1,52 @@
 /** @type {import('svdoc/types/svdoc.config').Config} */
 const config = {
-	name: "Shadeup",
-	icon: "img/logo.svg",
-	primary: "#C953DD",
-	external: [
-		{
-			href: "https://discord.gg/unreal-slackers",
-			text: "Discord",
-			icon: ":discord",
-		},
-		{
-			href: "https://unrealengine.com",
-			text: "Unreal Engine",
-			icon: ":unreal",
-		},
-		{
-			href: "https://github.com/AskingQuestions/shadeup",
-			text: "GitHub",
-			icon: ":github",
-		},
-	],
-	sidenav: [
-		{
-			href: "/docs",
-			text: "Overview",
-			icon: ":home",
-		},
-		{
-			href: "/docs/compute",
-			text: "Compute Shaders",
-			icon: ":numbers",
-		},
-		{
-			href: "/docs/nodes",
-			text: "Custom Nodes",
-			icon: ":graph",
-		},
-		{
-			href: "/docs/instancing",
-			text: "Indirect Instancing",
-			icon: ":grid",
-		},
-		{
-			href: "/docs/proxies",
-			text: "Proxies/Vertex Factories",
-			icon: "img/proxies-icon.svg",
-		},
-	],
+  name: "Shadeup",
+  icon: "img/logo.svg",
+  primary: "#C953DD",
+  external: [
+    {
+      href: "https://discord.gg/FfTtRMhDAS",
+      text: "Discord",
+      icon: ":discord",
+    },
+    {
+      href: "https://unrealengine.com",
+      text: "Unreal Engine",
+      icon: ":unreal",
+    },
+    {
+      href: "https://github.com/AskingQuestions/shadeup",
+      text: "GitHub",
+      icon: ":github",
+    },
+  ],
+  sidenav: [
+    {
+      href: "/docs",
+      text: "Overview",
+      icon: ":home",
+    },
+    {
+      href: "/docs/compute",
+      text: "Compute Shaders",
+      icon: ":numbers",
+    },
+    {
+      href: "/docs/nodes",
+      text: "Custom Nodes",
+      icon: ":graph",
+    },
+    {
+      href: "/docs/instancing",
+      text: "Indirect Instancing",
+      icon: ":grid",
+    },
+    {
+      href: "/docs/proxies",
+      text: "Proxies/Vertex Factories",
+      icon: "img/proxies-icon.svg",
+    },
+  ],
 };
 
 module.exports = config;
